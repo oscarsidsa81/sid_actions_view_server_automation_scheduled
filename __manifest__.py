8b2fb5eda24f08b83737510f2a05f1bc717bc56c
oscarsidsa81 <ovilches@sid-sa.com>
2025-07-01 16:43:40 +0200

    versión draft

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'sid_actions_view_server_automation_scheduled',
+    'version': '1.0',
+    'category': 'Technical Settings',
+    'summary': 'Acciones de todo el sistema',
+    'description': 'Módulo con todas las acciones de todos los tipos de v15 personalizadas',
+    'author': 'oscarsidsa81',
+    'depends': ['base'],
+    'data': [
+        'data/sid_view_actions.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
